18a6346c0798326127136a2829b04e6c871915ee
Ashitosh Katale <kataleashitosh123@gmail.com>
2025-11-22 13:36:20 +0530

    changed @Configuration to @Value

--- src/main/java/com/journal/Journal/service/WeatherService.java
+++ src/main/java/com/journal/Journal/service/WeatherService.java
@@ -1,8 +1,8 @@
 package com.journal.Journal.service;
 
 import com.journal.Journal.api.response.WeatherApiResponse;
-import com.journal.Journal.config.WeatherConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,13 +10,12 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class WeatherService {
-    @Autowired
-    private WeatherConfig weatherConfig;
+    @Value("${weather.api.key}")
+    private String weatherApiKey;
     @Autowired
     private RestTemplate restTemplate;
     private static final String fullUrl = "https://api.openweathermap.org/data/2.5/weather?q=CITY&appid=API_KEY";
     public WeatherApiResponse getWeatherInfo(String city) {
-        String weatherApiKey = weatherConfig.getKey();
         String finalApi = fullUrl.replace("CITY",city).replace("API_KEY",weatherApiKey);
         ResponseEntity<WeatherApiResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET,null, WeatherApiResponse.class);
         WeatherApiResponse body = response.getBody();
