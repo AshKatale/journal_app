3b57b072ad1524dde69c9ee58045541843cd25ba
Ashitosh Katale <kataleashitosh123@gmail.com>
2025-12-13 01:07:52 +0530

    Added Redis

--- src/main/java/com/journal/Journal/service/WeatherService.java
+++ src/main/java/com/journal/Journal/service/WeatherService.java
@@ -18,10 +18,21 @@ public class WeatherService {
     private RestTemplate restTemplate;
     @Autowired
     private AppCache appCache;
+    @Autowired
+    private RedisService redisService;
     public WeatherApiResponse getWeatherInfo(String city) {
-        String finalApi = appCache.APP_CACHE.get(AppCache.keys.WEATHER_URI.toString()).replace(Placeholders.CITY,city).replace(Placeholders.API_KEY,appCache.APP_CACHE.get(AppCache.keys.WEATHER_API_KEY.toString()));
-        ResponseEntity<WeatherApiResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET,null, WeatherApiResponse.class);
-        WeatherApiResponse body = response.getBody();
-        return body;
+        WeatherApiResponse weatherApiResponse = redisService.get("weather_of_"+city,WeatherApiResponse.class);
+        if(weatherApiResponse!=null) {
+            return weatherApiResponse;
+        }
+        else {
+            String finalApi = appCache.APP_CACHE.get(AppCache.keys.WEATHER_URI.toString()).replace(Placeholders.CITY,city).replace(Placeholders.API_KEY,appCache.APP_CACHE.get(AppCache.keys.WEATHER_API_KEY.toString()));
+            ResponseEntity<WeatherApiResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET,null, WeatherApiResponse.class);
+            WeatherApiResponse body = response.getBody();
+            if(body!=null) {
+                redisService.set("weather_of_"+city,body,300L);
+            }
+            return body;
+        }
     }
 }
